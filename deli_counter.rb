8d9a73ef0849f820b097d9b1c81b92095048990d
Daniel Sullivan <dansullivan19@gmail.com>
2019-05-21 16:35:29 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(array)
   if array.empty?
     puts "The line is currently empty."
   else
-    array.each_with_index { |val,index| 
+    puts "The line is currently #{array.each_with_index { |val,index| }
       
   end
 end
