2d45471762ff7f52fe8ef133eac008977075ced3
Daniel Sullivan <dansullivan19@gmail.com>
2019-05-21 16:32:00 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,19 @@
 # Write your code here.
+katz_deli = []
+
+def line(array)
+  if array.empty?
+    puts "The line is currently empty."
+  else
+    puts array
+  end
+end
+
+def take_a_number(array, person)
+  if array.empty?
+    array << person
+  else    
+    array.length > 0
+    array.push(person)
+  end
+end
